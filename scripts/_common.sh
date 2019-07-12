b230fbf49a5fa316b37459b84c212b8e8e5f5871
Anton Strömkvist <anton@stromkvist.com>
2019-07-12 04:12:50 +0200

    Add mysql-client dependency

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="ruby bundler"
+pkg_dependencies="ruby bundler default-libmysqlclient-dev"
 
 #=================================================
 # PERSONAL HELPERS
